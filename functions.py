db0f84dbc35fa7d1ae06e38a2af11ce7a1753e20
sewesely <sewesley@calpoly.edu>
2026-03-04 21:19:48 -0800

    AutoFilter()

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import data_processing
 
+data = data_processing.load_auto_trends("AutomotiveTrendsData1975-2024(Prelim).csv")
+
 
 #Purpose: Takes the data list and a year returns the average MPG is for that year.
 #Input: list[AutoTrendEntry], str
@@ -18,3 +20,41 @@ def avg_mpg_by_year(data:list[data_processing.AutoTrendEntry], year):
             total_mpg += vehicle.mpg
             vehicle_counter += 1
     return total_mpg/vehicle_counter
+
+
+#AutoFilter() - Sebastian Wesley
+#Purpose: Using a provided "field" & "filter", returns a list of AutoTrendEntry classes that match
+#Input: (list, str, str)
+#Output: list
+#Ex.: An input of (list[data_processing.AutoTrendEntry], "model_year", "1975") would returna list of AutoTrendEntry
+    #that are only of the 1975
+#How I'd complete as a computer?
+    # I would iterate through each element in the list
+    # check if the element has the attributes "field" and is equal to "filter"
+    # if true then append to a new list
+    # return the new list
+def AutoFilter(data:list, field:str, filter:str)->list[data_processing.AutoTrendEntry]:
+    filtered_data = []
+    for Auto in data:
+        if getattr(Auto,field) == filter:
+            filtered_data.append(Auto)
+    return filtered_data
+
+
+#GreaterThan() - Sebastian Wesley
+#Purpose: Using a provided list, "field", and float, returns a list of AutoTrendEntry classes that match
+#Input: (list, str, str)
+#Output: list
+#Ex.: An input of (list[data_processing.AutoTrendEntry], "model_year", "1975") would returna list of AutoTrendEntry
+    #that are only of the 1975
+#How I'd complete as a computer?
+    # I would iterate through each element in the list
+    # check if the element has the attributes "field" and is equal to "filter"
+    # if true then append to a new list
+    # return the new list
+def AutoFilter(data:list, field:str, filter:str)->list[data_processing.AutoTrendEntry]:
+    filtered_data = []
+    for Auto in data:
+        if getattr(Auto,field) == filter:
+            filtered_data.append(Auto)
+    return filtered_data
